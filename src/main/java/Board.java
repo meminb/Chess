bad77c5ef803bfee183e583b671c41d4cfd2ad82
oksana <oksanadamarja@gmail.com>
2021-06-24 22:12:26 +0300

    method inserted

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -79,6 +79,19 @@ public class Board implements Cloneable{
 
     }
 
+
+
+
+
+    public void calculateValueOfBoard(){
+
+    }
+
+
+
+
+
+
     public BufferedImage getBoard() {
         return ImageOfPieces.imagesInstance.getBoard();
     }
